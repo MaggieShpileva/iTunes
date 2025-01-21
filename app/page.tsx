7c1afb6f8ad0ef3bf0f2875482aaea6becc8fc8d
MaggieShpileva <63877500+MaggieShpileva@users.noreply.github.com>
2025-01-21 12:34:56 +0300

    Деплой приложения

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ import Loader from './lib/components/Loader'
 
 export default function Home() {
     const [items, setItems] = useState<CommonItem[] | null>(null)
-    const [isLoading, setIsLoading] = useState(true)
+    const [isLoading, setIsLoading] = useState(false)
     return (
         <>
             <SearchBar setItems={setItems} setIsLoading={setIsLoading} />
